1179a95d1023d53c79cb2d2b42f3b9424de00b9f
advnpzn <adenosinetp10@protonmail.com>
2025-05-18 09:17:08 +0530

    chore: remove .idea/, add ci

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
-#include "chip8.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/VideoMode.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto window = sf::RenderWindow(sf::VideoMode({640, 400}), "Chip8 Emulator");
+    window.setFramerateLimit(144);
+
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Cyan);
+        window.display();
+    }
+
     return 0;
 }
