7a5e4f2d0622a5f55702f59b311106ecdee18c62
advnpzn <advnpzn@proton.me>
2026-06-23 23:54:07 +0530

    style: apply clang-format to source files

--- include/chip8/bus.hpp
+++ include/chip8/bus.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <array>
-#include <cstdint>
-
 #include "chip8/constants.hpp"
 
-
+#include <array>
+#include <cstdint>
 
 struct Bus {
 
     std::array<uint8_t, MEMORY_SIZE> memory;
     std::array<bool, DISPLAY_WIDTH * DISPLAY_HEIGHT> display_buffer;
     std::array<bool, KEYPAD_SIZE> keypad;
-
 };
